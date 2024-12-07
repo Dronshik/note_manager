cb87fdbadc0759db125ed622fdf96b7010acd006
Dronshik <nii200116@gmail.com>
2024-12-07 17:36:59 +0300

    Update final.py
    fix end

--- final.py
+++ final.py
@@ -6,5 +6,6 @@ content = input("Описание: ")
 status = input("Статус работы: ")
 created_date = input("Начало работы (дд.мм.гг.): ")
 issue_date = input("Окончание работы (дд.мм.гг.): ")
-list = [user_name, title1, title2, title3, content, status, created_date, issue_date]
-print ("Здравствуйте,", list[0], '!', "Ваша тема:", (list[1]), ',', (list[2]), 'и', (list[3]), '.', "Описание:", list[4], '.',  "Статус работы:", list[5],"Дата выполнения с", list[6], "по", list[7], '.')
+list = [title1, title2, title3]
+book = {1:user_name, 2:(list[0]), 3:(list[1]), 4:(list[2]), 5:content, 6:status, 7:created_date, 8:issue_date }
+print("Здравствуйте,", book[1], '!', "Ваша тема:", book[2], ',', book[3], 'и', book[4], '.',  "Описание:",book[3], '.',  "Статус работы:", book[6], "Дата выполнения с", book[7], "по", book[8])
