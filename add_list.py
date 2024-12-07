3d2abf8f0266d6d2261f085aa41339188a679679
Dronshik <nii200116@gmail.com>
2024-12-07 16:56:49 +0300

    fix

--- add_list.py
+++ add_list.py
@@ -1,11 +1,11 @@
 user_name = input("Введите имя пользователя: ")
-title = input("Заголовок 1: ")
-title = input("Заголовок 2: ")
-title = input("Заголовок 2: ")
+title1 = input("Заголовок 1: ")
+title2 = input("Заголовок 2: ")
+title3 = input("Заголовок 2: ")
 content = input("Описание: ")
 status = input("Статус работы: ")
 created_date = input("Начало работы (дд.мм.гг.): ")
 issue_date = input("Окончание работы (дд.мм.гг.): ")
-print ((user_name),(title),(content),(status),(created_date),(issue_date))
-tuple = title
-print(tuple)
+list = [title1, title2, title3]
+print ("Здравствуйте,",(user_name),'Ваши темы:',(list[0]), ',', (list[1]), 'и', (list[2]), "Описание:", (content),'статус работы:', (status), 'Дата начала:',(created_date), 'Дата окончани:',(issue_date))
+
